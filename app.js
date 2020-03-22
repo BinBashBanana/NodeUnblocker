6329ffd7305cdf6f4f04b1284a51314cbfc04d55
BinBashBanana <51469593+BinBashBanana@users.noreply.github.com>
2020-03-21 23:30:55 -0700

    002

--- app.js
+++ app.js
@@ -27,10 +27,7 @@ function injectScript(data) {
 				.replace('_blank','_self')
 				.replace('_parent','_self')
 				.replace('_top','_self')
-				.replace('</body>',`<script src="//code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script><script>
-eval(unescape(atob("dmFyIG92ZXJsYXk9ITA7JChkb2N1bWVudCkua2V5ZG93bihmdW5jdGlvbihlKXt2YXIgbD10b3A7IjE3Ij09ZS53aGljaD9jbnRybElzUHJlc3NlZD0hMDo3Nz09ZS53aGljaCYmY250cmxJc1ByZXNzZWQ/KCJub25lIiE9PWwuZG9jdW1lbnQuZ2V0RWxlbWVudEJ5SWQoInJ1c2ljLW1vZGFsIikuc3R5bGUuZGlzcGxheT8obC5kb2N1bWVudC5nZXRFbGVtZW50QnlJZCgicnVzaWMtbW9kYWwiKS5zdHlsZS5kaXNwbGF5PSJub25lIiwhMD09PW92ZXJsYXkmJihsLmRvY3VtZW50LmdldEVsZW1lbnRzQnlDbGFzc05hbWUoIm92ZXJsYXkiKVswXS5zdHlsZS5kaXNwbGF5PSJub25lIikpOihsLmRvY3VtZW50LmdldEVsZW1lbnRCeUlkKCJydXNpYy1tb2RhbCIpLnN0eWxlLmRpc3BsYXk9ImluaXRpYWwiLCEwPT09b3ZlcmxheSYmKGwuZG9jdW1lbnQuZ2V0RWxlbWVudHNCeUNsYXNzTmFtZSgib3ZlcmxheSIpWzBdLnN0eWxlLmRpc3BsYXk9ImluaXRpYWwiKSksY250cmxJc1ByZXNzZWQ9ITEpOmNudHJsSXNQcmVzc2VkPSExfSk7")));
-function gcloak() { var link = document.querySelector("link[rel*='icon']") || document.createElement('link'); link.type = 'image/x-icon'; link.rel = 'shortcut icon'; link.href = 'https://google.com/favicon.ico'; document.title = 'Google'; document.getElementsByTagName('head')[0].appendChild(link) }; gcloak();setInterval(gcloak, 1000); 
-</script></body>`)
+				.replace('</body>',`<script src="//code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script></body>`)
 				this.push(updated, "utf8");
 				next();
 			}
